53c20c774057523e903603be2bd018ac948a34ed
Willian <will.hayashida07@gmail.com>
2018-10-12 20:17:29 -0300

    m

--- lib/postprocess/pings.sh
+++ lib/postprocess/pings.sh
@@ -3,5 +3,19 @@
 
 for folder in $(ls)
 do
-  BASE=ls "${folder}/pings" | tail -n 1
-  ls $(sed 's/\_[^\_]*$//' <<< ${BASE} )*
+  BASE=$(ls "${folder}/pings" | tail -n 1)
+  ls $(sed 's/\_[^\_]*$//' <<< ${BASE} )* > IPS
+  sed -i 's/^.*\_[^\_]/1/' IPS
+
+  for i in $(cat IPS)
+  do
+    for j in $(cat IPS)
+    do
+      i_aux=$(sed 's/\./-/g' <<< $i)
+
+    done
+  done
+done
+
+
+ping_ip-10-0-0-241_to_10.0.0.241

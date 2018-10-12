4300b6530f9a38dc56f163b425caf6ff735a5ec6
Willian <will.hayashida07@gmail.com>
2018-10-12 15:32:58 -0300

    m

--- run_test.py
+++ run_test.py
@@ -178,9 +178,10 @@ def main():
     ip = instances[0].public_ip_address
 
     for instance in instances:
-        os.system('ssh-keygen -R %s' % ip)
-        os.system('ssh-keyscan -H %s >> ~/.ssh/known_hosts' % ip)
-        os.system('scp -r -i "willkey.pem" ubuntu@%s:pings/* %s' % (instance.public_ip, result_dir+'/pings'))
+        os.system('ssh-keygen -R %s' % instance.public_ip_address)
+        os.system('ssh-keyscan -H %s >> ~/.ssh/known_hosts' % instance.public_ip_address)
+        os.system('mkdir -p %s' % result_dir+'/pings')
+        os.system('scp -r -i "willkey.pem" ubuntu@%s:pings/* %s' % (instance.public_ip_address, result_dir+'/pings'))
 
     # os.system('scp -r -i "willkey.pem" ubuntu@%s:pings %s' % (ip, result_dir))
 

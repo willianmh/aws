79ec18a1f3148b48c61baa59ccc764752ef5c2c9
Willian <will.hayashida07@gmail.com>
2018-10-13 13:52:05 -0300

    m

--- lib/postprocess/get_times.sh
+++ lib/postprocess/get_times.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 DIR=$1
-OUTPUT_FILE=$DIR/times
+OUTPUT_FILE=$DIR/all_times
 rm $OUTPUT_FILE
-
+rm $DIR/a $DIR/b $DIR/time.csv
 for folder in $(ls ${DIR})
 do
-	echo ${DIR}/$folder >> $OUTPUT_FILE
-	cat ${DIR}/${folder}/inversion.out| tail -n 100 | sed -e '/summary/,$!d' -e '/END OF PROGRAM/,+1 d' >> $OUTPUT_FILE
+	echo ${DIR}/$folder  >> $OUTPUT_FILE
+	cat ${DIR}/${folder}/inversion.out | tail -n 350 | sed -e '/summary/,$!d' -e '/END OF PROGRAM/,+1 d' >> $OUTPUT_FILE
 	echo '' >> $OUTPUT_FILE
 done
+
+grep 'results_cl_1' $OUTPUT_FILE | sed 's/results_cl_1\///g' > $DIR/a
+grep 'Average' $OUTPUT_FILE > $DIR/b
+
+sed -i 's/|[^|]*$//;s/ //g;s/Average|//;s/[|].*$//g' $DIR/b
+paste -d, $DIR/a $DIR/b > $DIR/time.csv
+rm $DIR/a $DIR/b

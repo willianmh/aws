c6abcff87a10748cb93ecaaaf308e4c906222617
Willian <will.hayashida07@gmail.com>
2018-10-13 02:23:27 -0300

    m

--- lib/postprocess/pings.sh
+++ lib/postprocess/pings.sh
@@ -3,28 +3,37 @@
 PATH_TO_DIR=$1
 OUTPUT_FILE=${PATH_TO_DIR}/latencies.out
 
-for folder in $(ls ${PATH_TO_DIR})
+
+for folder in $(ls $PATH_TO_DIR)
 do
-  BASE=$(ls "${folder}/pings" | tail -n 1)
-  ls $(sed 's/\_[^\_]*$//' <<< ${BASE} )* > IPS
-  sed -i 's/^.*\_[^\_]/1/' IPS
+  if [ -d "${PATH_TO_DIR}/${folder}" ]
+  then
+    PING_DIR=${PATH_TO_DIR}/${folder}/pings
+    BASE=$(ls ${PING_DIR} | tail -n 1)
+    ls ${PING_DIR}/$(sed 's/\_[^\_]*$//' <<< ${BASE} )* > IPS
+    sed -i 's/^.*\_[^\_]/1/' IPS
 
-  echo $folder | tr '\n' ',' >> ${OUTPUT_FILE}
-  for j in $(cat IPS)
-  do
-    echo $j | tr '\n' ',' >> ${OUTPUT_FILE}
-  done
-
-  echo '' >> ${OUTPUT_FILE}
-
-  for i in $(cat IPS)
-  do
-    echo $i | tr '\n' ',' >> ${OUTPUT_FILE}
+    echo $folder | tr '\n' ',' >> ${OUTPUT_FILE}
     for j in $(cat IPS)
     do
-      i_aux=$(sed 's/\./-/g' <<< $i) # 10-0-0-95
-      AVG_PING=$(grep avg ping_ip-${i_aux}_to_${j} | sed -e 's/^.*= //' -e 's/^[^\/]*\///g' -e 's/[\/].*$//')
-      echo ${AVG_PING} | tr '\n' ',' >> $OUTPUT_FILE
+      echo $j | tr '\n' ',' >> ${OUTPUT_FILE}
     done
-  done
+
+
+    echo '' >> ${OUTPUT_FILE}
+
+    for i in $(cat IPS)
+    do
+      echo $i | tr '\n' ',' >> ${OUTPUT_FILE}
+      for j in $(cat IPS)
+      do
+        i_aux=$(sed 's/\./-/g' <<< $i) # 10-0-0-95
+        grep avg ${PING_DIR}/ping_ip-${i_aux}_to_${j}
+        AVG_PING=$(grep avg ${PING_DIR}/ping_ip-${i_aux}_to_${j} | \
+          sed -e 's/^.*= //' -e 's/^[^\/]*\///g' -e 's/[\/].*$//')
+        echo ${AVG_PING} | tr '\n' ',' >> $OUTPUT_FILE
+      done
+      echo '' >> ${OUTPUT_FILE}
+    done
+  fi
 done

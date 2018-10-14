0d4186b882dbb7cfd1018735df9597c33f3edcf7
Willian <will.hayashida07@gmail.com>
2018-10-14 01:11:49 -0300

    m

--- lib/postprocess/get_times.sh
+++ lib/postprocess/get_times.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
 DIR=$1
-OUTPUT_FILE=$DIR/all_times
-rm $OUTPUT_FILE
-rm $DIR/a $DIR/b $DIR/time.csv
-for folder in $(ls ${DIR})
+
+# generate one file for each case (1 & 2)
+for i in `seq 1 2`
 do
-	echo ${DIR}/$folder  >> $OUTPUT_FILE
-	cat ${DIR}/${folder}/inversion.out | tail -n 350 | sed -e '/summary/,$!d' -e '/END OF PROGRAM/,+1 d' >> $OUTPUT_FILE
-	echo '' >> $OUTPUT_FILE
+  OUTPUT_FILE=$DIR/all_times_${i}
+  rm $OUTPUT_FILE
+  # iterate under each item in dir
+  for folder in $(ls ${DIR})
+  do
+    if [ -d "$folder" ]
+    then
+      for j in `seq 1 5`
+      do
+        echo ${DIR}/$folder  >> $OUTPUT_FILE
+    	  cat ${DIR}/${folder}/inversion_${i}_${j}.out | tail -n 350 | sed -e '/summary/,$!d' -e '/END OF PROGRAM/,+1 d' >> $OUTPUT_FILE
+    	  echo '' >> $OUTPUT_FILE
+      done
+    fi
+  done
+  grep 'results_*' $OUTPUT_FILE | sed 's/results_*\///g' > $DIR/a
+  grep 'Average' $OUTPUT_FILE > $DIR/b
+
+  sed -i 's/|[^|]*$//;s/ //g;s/Average|//;s/[|].*$//g' $DIR/b
+  paste -d, $DIR/a $DIR/b > $DIR/time_${i}.csv
 done
 
-grep 'results_*' $OUTPUT_FILE | sed 's/results_*\///g' > $DIR/a
-grep 'Average' $OUTPUT_FILE > $DIR/b
 
-sed -i 's/|[^|]*$//;s/ //g;s/Average|//;s/[|].*$//g' $DIR/b
-paste -d, $DIR/a $DIR/b > $DIR/time.csv
-rm $DIR/a $DIR/b
+
+
+rm -f $DIR/a $DIR/b
